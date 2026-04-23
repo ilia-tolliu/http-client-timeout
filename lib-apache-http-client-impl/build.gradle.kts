90352a8a542c09c00aff91c349d561fe2a66dc2e
Ilia Tolliu <ilia.tolliu@etraveligroup.com>
2026-04-23 16:02:08 +0200

    add ApacheClientImpl

--- lib-apache-http-client-impl/build.gradle.kts
+++ lib-apache-http-client-impl/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
 }
 
 dependencies {
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
 }
--- lib-apache-http-client-impl/src/main/java/example/client/impl/apache/ApacheClientImpl.java
+++ lib-apache-http-client-impl/src/main/java/example/client/impl/apache/ApacheClientImpl.java
@@ -1,4 +1,43 @@
 package example.client.impl.apache;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.time.Duration;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 public class ApacheClientImpl {
+
+  public void getSlowResource(Duration requestTimeout) {
+    try (var client = HttpClientBuilder.create().build()) {
+      var request = new HttpGet(URI.create("http://localhost:8000/slow"));
+      var requestConfig = RequestConfig.custom()
+        .setConnectionRequestTimeout(requestTimeout.toMillisPart())
+        .build();
+      request.setConfig(requestConfig);
+
+      client.execute(request, response -> {
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode != 200) {
+          throw new RuntimeException("Response is not Ok: %d".formatted(statusCode));
+        }
+
+        try(
+          var inputStream = response.getEntity().getContent();
+          var inputStreamReader = new InputStreamReader(inputStream);
+          var lineReader = new BufferedReader(inputStreamReader)
+        ) {
+          lineReader.lines().forEach(System.out::println);
+        }
+
+        return statusCode;
+      });
+    } catch (IOException e) {
+      throw new RuntimeException("Request failed", e);
+    }
+  }
 }
