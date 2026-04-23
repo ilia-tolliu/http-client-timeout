dfaa58da6c6afecc8a00e27596f22788a31d95a9
Ilia Tolliu <ilia.tolliu@etraveligroup.com>
2026-04-23 16:01:58 +0200

    add JdkClientImpl

--- lib-jdk-http-client-impl/src/main/java/example/client/impl/jdk/JdkClientImpl.java
+++ lib-jdk-http-client-impl/src/main/java/example/client/impl/jdk/JdkClientImpl.java
@@ -1,4 +1,31 @@
 package example.client.impl.jdk;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+
 public class JdkClientImpl {
+  public void getSlowResource(Duration requestTimeout) {
+    try (var client = HttpClient.newBuilder().build()) {
+      var request = HttpRequest.newBuilder()
+        .uri(URI.create("http://localhost:8000/slow"))
+        .timeout(requestTimeout)
+        .build();
+
+      var response = client.send(request, BodyHandlers.ofLines());
+
+      int statusCode = response.statusCode();
+      if (statusCode != 200) {
+        throw new RuntimeException("Response is not Ok: %d".formatted(statusCode));
+      }
+
+      response.body().forEach(System.out::println);
+    } catch (IOException | InterruptedException e) {
+      throw new RuntimeException("Request failed", e);
+    }
+  }
+
 }
