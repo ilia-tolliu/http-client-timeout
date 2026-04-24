f9380a2ab59c64dd55572be09e50eb58ef296488
Ilia Tolliu <ilia.tolliu@etraveligroup.com>
2026-04-24 11:12:18 +0200

    implement full request timeout for JDK HTTP Client

--- app-client/src/main/java/example/client/app/ClientApp.java
+++ app-client/src/main/java/example/client/app/ClientApp.java
@@ -1,16 +1,19 @@
 package example.client.app;
 
+import java.net.http.HttpClient;
 import java.time.Duration;
 import java.time.Instant;
 
 import example.client.impl.apache.ApacheClientImpl;
 import example.client.impl.jdk.JdkClientImpl;
+import example.client.impl.jdk.JdkClientImpl.RequestTimeoutException;
 
 public class ClientApp {
 
-  private static final Duration REQUEST_TIMEOUT = Duration.ofMillis(60);
+  private static final Duration READ_TIMEOUT = Duration.ofMillis(200);
+  private static final Duration REQUEST_TIMEOUT = Duration.ofSeconds(3);
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     try {
       runWithJdkClient();
     } catch (Exception e) {
@@ -24,18 +27,22 @@ public class ClientApp {
       System.err.println("Apache HTTP Client failed");
       e.printStackTrace(System.err);
     }
+
+    Thread.sleep(Duration.ofSeconds(10));
   }
 
   static void runWithJdkClient() {
     System.out.println("Running with JDK HTTP Client\n");
 
-    var jdkClientImpl = new JdkClientImpl();
-    var jdkClientStart = Instant.now();
+    try (var client = HttpClient.newBuilder().build()) {
+      var jdkClientImpl = new JdkClientImpl(client);
+      var response = jdkClientImpl.getSlowResource(READ_TIMEOUT, REQUEST_TIMEOUT);
 
-    jdkClientImpl.getSlowResource(REQUEST_TIMEOUT);
-
-    var jdkClientElapsed = Duration.between(jdkClientStart, Instant.now());
-    System.out.printf("%n%nJDK HTTP Client processed the request in %s%n%n", jdkClientElapsed);
+      System.out.printf("%n%nJDK HTTP Client processed complete request in %s%n%n", response.duration());
+    } catch (RequestTimeoutException e) {
+      System.out.printf("%n%nJDK HTTP Client got incomplete request in %s%n%n", e.getIncompleteResponse().duration());
+      throw e;
+    }
   }
 
   static void runWithApacheClient() {
--- lib-jdk-http-client-impl/src/main/java/example/client/impl/jdk/JdkClientImpl.java
+++ lib-jdk-http-client-impl/src/main/java/example/client/impl/jdk/JdkClientImpl.java
@@ -1,31 +1,156 @@
 package example.client.impl.jdk;
 
+import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.Temporal;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Objects.isNull;
 
 public class JdkClientImpl {
-  public void getSlowResource(Duration requestTimeout) {
-    try (var client = HttpClient.newBuilder().build()) {
-      var request = HttpRequest.newBuilder()
-        .uri(URI.create("http://localhost:8000/slow"))
-        .timeout(requestTimeout)
-        .build();
+  private final HttpClient client;
 
-      var response = client.send(request, BodyHandlers.ofLines());
+  public JdkClientImpl(HttpClient client) {
+    this.client = client;
+  }
 
-      int statusCode = response.statusCode();
-      if (statusCode != 200) {
-        throw new RuntimeException("Response is not Ok: %d".formatted(statusCode));
+  public CompletedResponse getSlowResource(
+    Duration firstByteTimeout,
+    Duration fullTimeout
+  ) {
+    var start = Instant.now();
+    var deadline = start.plus(fullTimeout);
+
+    var request = HttpRequest.newBuilder()
+      .uri(URI.create("http://localhost:8000/slow"))
+      .timeout(firstByteTimeout)
+      .build();
+
+    try {
+      var httpResponse = client.send(request, BodyHandlers.ofInputStream());
+
+      int statusCode = httpResponse.statusCode();
+      var bodyBuilder = new StringBuilder();
+
+      var canceller = new ReadCanceller();
+
+      try (
+        var in = httpResponse.body();
+        canceller;
+        var streamReader = new InputStreamReader(in);
+        var lineReader = new BufferedReader(streamReader)
+      ) {
+        canceller.start(in, deadline);
+
+        String line;
+        while (true) {
+          line = lineReader.readLine();
+          if (isNull(line)) {
+            break;
+          }
+
+          bodyBuilder.append(line);
+          System.out.println(line);
+        }
+      } catch (IOException e) {
+        if (canceller.isCancelled()) {
+          var response = new IncompleteResponse(statusCode, bodyBuilder.toString(), durationFrom(start));
+          throw new RequestTimeoutException(response, e);
+        }
+
+        throw e;
       }
 
-      response.body().forEach(System.out::println);
+      return new CompletedResponse(statusCode, bodyBuilder.toString(), durationFrom(start));
     } catch (IOException | InterruptedException e) {
-      throw new RuntimeException("Request failed", e);
+      throw new RuntimeException(e);
     }
   }
 
+  private Duration durationFrom(Temporal start) {
+    return Duration.between(start, Instant.now());
+  }
+
+  public record CompletedResponse(
+    int statusCode,
+    String body,
+    Duration duration
+  ) {
+  }
+
+  public record IncompleteResponse(
+    int statusCode,
+    String incompleteBody,
+    Duration duration
+  ) {
+  }
+
+  public static class RequestTimeoutException extends RuntimeException {
+    private final IncompleteResponse incompleteResponse;
+
+    public RequestTimeoutException(IncompleteResponse incompleteResponse,  Exception cause) {
+      super(cause);
+
+      this.incompleteResponse = incompleteResponse;
+    }
+
+    public IncompleteResponse getIncompleteResponse() {
+      return incompleteResponse;
+    }
+  }
+
+}
+
+class ReadCanceller implements AutoCloseable {
+  private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+  private ScheduledFuture<?> future  = null;
+  private volatile boolean isCancelled = false;
+
+  void start(Closeable cancellable, Temporal deadline) {
+    var duration = Duration.between(Instant.now(), deadline);
+    if (!duration.isPositive()) {
+      cancel(cancellable);
+      return;
+    }
+
+    future = scheduler.schedule(
+      () -> cancel(cancellable),
+      duration.toMillis(),
+      TimeUnit.MILLISECONDS
+    );
+  }
+
+  private void cancel(Closeable cancellable) {
+    try {
+      isCancelled = true;
+      cancellable.close();
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to cancel", e);
+    }
+  }
+
+  boolean isCancelled() {
+    return isCancelled;
+  }
+
+  @Override
+  public void close() {
+    if (isNull(future)) {
+      return;
+    }
+
+    future.cancel(false);
+  }
 }
